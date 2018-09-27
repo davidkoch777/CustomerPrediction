e1e7582354154fe7468b02c4f16b792e0e1ade81
davidkoch777 <david.koch.1992@gmail.com>
2018-09-27 23:28:18 +0200

    added amount of employees random

--- CleanData.py
+++ CleanData.py
@@ -1,6 +1,6 @@
 import csv
 import numpy as np
-from numpy.random import choice
+from numpy.random import choice, randint
 
 
 laender = range(5)
@@ -19,12 +19,17 @@ branchenschluessel[0] = "Industrie"
 branchenschluessel[1] = "Bauwesen"
 branchenschluessel[2] = "Energie"
 
-branchendistproland = [[0 for x in range(3)] for y in range(5)]
+branchendistproland = [[0 for x in range(len(branchen))] for y in range(len(laender))]
 branchendistproland[0] = [0.6, 0.2, 0.2]
 branchendistproland[1] = [0.15, 0.15, 0.7]
 branchendistproland[2] = [0.5, 0.25, 0.25]
 branchendistproland[3] = [0.55, 0.2, 0.25]
 branchendistproland[4] = [0.45, 0.1, 0.45]
+
+mitarbeiterrangeprobranche = [[0 for x in range(2)] for y in range(len(branchen))]
+mitarbeiterrangeprobranche[0] = [1000, 20000]
+mitarbeiterrangeprobranche[1] = [100, 2000]
+mitarbeiterrangeprobranche[2] = [250, 5000]
 
 
 def clean_data(my_data):
@@ -34,24 +39,43 @@ def clean_data(my_data):
 
         laenderdistribution = choice(laender, row_count, p=laenderdist)
 
-        pairs = []
+        dataset = []
 
         i = 0
         file.seek(0)
         for row in reader:
-            pos1 = row[1].index('(')
-            pos2 = row[1].index(')')
-            row[0] = i+1
-            row[1] = row[1][:pos1] + row[1][pos2+1:]
-            row[1] = row[1].strip()
-            row[2] = laenderdistribution[i]
+            # URL aus dem Namen schneiden
+            # pos1 = row[1].index('(')
+            # pos2 = row[1].index(')')
+            # row[1] = row[1][:pos1] + row[1][pos2+1:]
+            # row[1] = row[1].strip()
 
             branche = choice(branchen, 1, p=branchendistproland[laenderdistribution[i]])
-            pair = [laenderdistribution[i], branche[0]]
-            pairs.append(pair)
- #          newRow = np.array([row, branchendistribution[i], businessdistribution[i]])
+            mitarbeiteranzahl = randint(mitarbeiterrangeprobranche[branche[0]][0]
+                                        , high=mitarbeiterrangeprobranche[branche[0]][1]+1)
+
+            if mitarbeiteranzahl == 0:
+                print(mitarbeiterrangeprobranche[branche[0]][0])
+                print(mitarbeiterrangeprobranche[branche[0]][1]+1)
+
+            row[0] = i + 1
+            row[1] = laenderdistribution[i]
+            row[2] = branche[0]
+            row[3] = mitarbeiteranzahl
+
+            growth = float(row[5].replace("%", ""))
+            if(growth >= 1000.0):
+                growth = round(growth / 100, 2)
+            if (growth >= 100.0):
+                growth = round(growth / 10, 2)
+            row[5] = growth
+
             i = i + 1
-        return pairs
+
+            dataset.append(row)
+#        save = np.array(dataset)
+        np.savetxt('C:/Users/David/Downloads/dataset.txt', dataset, delimiter=',', fmt="%s")
+
 
 def check_distribution(pairs):
 
@@ -70,11 +94,7 @@ def check_distribution(pairs):
                   , " | Plan: ", branchendistproland[i][j])
         print()
 
-pairs = clean_data('C:/Users/David/Downloads/company_data.csv')
-check_distribution(pairs)
-
-# save = np.array(pairs)
-# np.savetxt('C:/Users/David/Downloads/test.txt', pairs, delimiter=',', fmt="%s")
+clean_data('C:/Users/David/Downloads/company_data.csv')
 
 # Numpy add column to 2-dim array:
 # x = np.array([[10,20,30], [40,50,60]])
